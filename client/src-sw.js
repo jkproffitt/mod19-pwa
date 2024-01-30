3e4b6e5fa600f29ceba59afcb6e40fc9aab5a2b7
jproff <jessicakproffitt@gmail.com>
2024-01-30 18:03:51 -0500

    initial structure

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,35 +7,41 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+pageCache();
+googleFontCache();
+offlineFallback();
+staticResourceCache();
+
 // Set up page cache
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ['/index.html', '/'],
+	strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
+	({ request }) =>
+		['style', 'script', 'worker'].includes(request.destination),
+	new StaleWhileRevalidate({
+		cacheName: 'asset-cache',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200],
+			}),
+		],
+	})
 );
